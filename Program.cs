2201ddb8f41ae9abc70f11e69799ad0ed8f41b05
luisqm <47574013+luisqm@users.noreply.github.com>
2019-09-06 12:57:46 -0700

    Add files via upload
    Trabajo de auto

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+// los metodos deben estar dentro de las clases
+
+namespace bici
+{
+     class Auto
+     {
+         private string marca;
+        
+         private int año;
+         private string modelo;
+
+         public void setMarca(string m)
+         {
+             marca=m;
+
+         }
+         public String getMarca()
+             {
+                return marca;
+             }
+
+             public void setModelo(string m)
+         {
+             modelo=m;
+
+         }
+         public String getModelo()
+             {
+                return modelo;
+             }
+
+              public void setAño(int a)
+         {
+             año=a;
+
+         }
+         public int getAño()
+             {
+                return año;
+             }
+
+             //Constructor
+             public Auto (string m, int a, string mod ) //No tienen que estar en orden a fuerzas
+             {
+                 this.marca=m;
+                 this.año=a;
+                 this.modelo=mod;
+             }
+
+            public Auto () 
+             {
+                 año=2020; //por default
+             }
+             //dos metodos que se llaman igual=sobrecarga de metodos
+
+             public void imprime ()
+             {
+                 
+                 Console.WriteLine(marca+" " + año+" "+ modelo+".");
+             }
+
+         
+     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto();
+            Auto auto1 = new Auto("Toyota", 2019, "B200");
+            
+            auto.setMarca("Ford");
+            auto.setAño(2019);
+             auto.setModelo("B100");
+            auto1.setMarca("Toyota");
+            auto1.setAño(2019);
+             auto1.setModelo("B200");
+             auto.imprime();
+             auto1.imprime(); 
+           
+           // Console.WriteLine(auto.getMarca());
+            //Console.WriteLine(auto.getModelo());
+            //Console.WriteLine(auto1.getMarca()+ auto1.getModelo()+ auto1.getAño()+".");
+
+        }
+    }
+}
